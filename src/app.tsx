949939694f3c2e2de069ccd8530a1ec6c4650b79
shivani maiyani <shivani.maiyani@scaletech.xyz>
2023-06-15 12:41:02 +0530

    base setup

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
+import "./assets/styles/app.scss";
+
+const App = () => {
+    return (
+        <Routes>
+            <Route path="/" element />
+        </Routes>
+    );
+};
+
+export default App;
